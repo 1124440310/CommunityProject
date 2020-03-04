638203bd44189c32309440c2d44f5f93d1ce3326
Reserved <1124440310@qq.com>
2020-03-04 19:47:06 +0800

    add index and bootstrap

--- src/main/java/com/chen/myproject/controller/IndexController.java
+++ src/main/java/com/chen/myproject/controller/IndexController.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("hello/{name}")
-    public String hello(@PathVariable("name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
